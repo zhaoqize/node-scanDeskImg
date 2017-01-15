e1493396009ff097ece414d40d2ff5fafd9060d1
zhaoqize <ko2338792975@163.com>
2017-01-15 20:43:45 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+var electron = require('electron');
+
+var app = electron.app;
+ 
+var BrowserWindow = electron.BrowserWindow;
+ 
+var mainWindow;
+ 
+function createWindow(){
+    //设置主进程窗口大小
+    mainWindow = new BrowserWindow({
+        //width:800,
+        //height:400,
+        //titleBarStyle: 'hidden' //mac上的样式
+        //transparent: true
+        //frame: false
+    })
+    
+    //主进程启动后打开的页面
+    mainWindow.loadURL(`file://${__dirname}/index.html`);
+    
+    //谷歌调试工具
+    mainWindow.webContents.openDevTools()
+    
+    //主进程关闭后触发的事件
+    mainWindow.on('closed', function () {
+ 
+        mainWindow = null;
+    })
+ 
+}
+
+//应用启动成功后触发ready
+app.on('ready',createWindow);
+
+//应用窗口关闭按钮
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+})
